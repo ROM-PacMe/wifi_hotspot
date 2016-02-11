dbb077574e32fe4eb6c7b75b4decfcf374a2c370
diewland <diewland@gmail.com>
2016-02-12 01:12:51 +0700

    modified:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/ic_launcher-web.png 	modified:   app/src/main/java/com/diewland/wi_fihotspot/MainActivity.java 	modified:   app/src/main/res/mipmap-hdpi/ic_launcher.png 	modified:   app/src/main/res/mipmap-mdpi/ic_launcher.png 	modified:   app/src/main/res/mipmap-xhdpi/ic_launcher.png 	modified:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	modified:   app/src/main/res/values/styles.xml

--- app/src/main/java/com/diewland/wi_fihotspot/MainActivity.java
+++ app/src/main/java/com/diewland/wi_fihotspot/MainActivity.java
@@ -1,15 +1,27 @@
 package com.diewland.wi_fihotspot;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ApManager.configApState(MainActivity.this);
+        try {
+            Toast.makeText(this, "Toggling Wi-Fi hotspot", Toast.LENGTH_SHORT).show();
+            ApManager.configApState(MainActivity.this);
+        }
+        catch(Exception e){
+            // if not work, jump to hotspot setting
+            Intent intent = new Intent();
+            intent.setClassName("com.android.settings", "com.android.settings.TetherSettings");
+            startActivity(intent);
+        }
 
         finish();
     }
